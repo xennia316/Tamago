afbda072f360f713692fbc4c28b794abf5e8d859
xennia316 <xennia316@gmail.com>
2022-07-08 19:43:04 +0100

    sidenav milestone

--- client/src/SideNav/SideNav.jsx
+++ client/src/SideNav/SideNav.jsx
@@ -1,16 +1,44 @@
 import React from "react";
 import styles from "./SideNav.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faShareFromSquare,
+  faHeart,
+  faBell,
+  faGear,
+  faArrowAltCircleLeft,
+} from "@fortawesome/free-solid-svg-icons";
 
 const SideNav = () => {
   return (
     <section className={styles.body}>
-      <section className={styles.content}>Home</section>
-      <section className={styles.content}>About</section>
-      <section className={styles.content}>Favorite</section>
-      <section className={styles.content}>Contact</section>
-      <section className={styles.content}>Bookings</section>
-      <section className={styles.content}>Settings</section>
-      <section className={styles.content}>Logout</section>
+      <section className={styles.top}>Tamago</section>
+      <section className={styles.content}>
+        <FontAwesomeIcon icon={faHome} className={styles.icon} />
+        Dashboard
+      </section>
+      <section className={styles.content}>
+        <FontAwesomeIcon icon={faShareFromSquare} className={styles.icon} />
+        Explore City
+      </section>
+      <section className={styles.content}>
+        <FontAwesomeIcon icon={faBell} className={styles.icon} />
+        Notification
+      </section>
+      <section className={styles.content}>
+        <FontAwesomeIcon icon={faHeart} className={styles.icon} />
+        Favorite
+      </section>
+      <section className={styles.content}>
+        <FontAwesomeIcon icon={faGear} className={styles.icon} />
+        Settings
+      </section>
+      <section></section>
+      <section className={styles.content}>
+        <FontAwesomeIcon icon={faArrowAltCircleLeft} className={styles.icon} />
+        Logout
+      </section>
     </section>
   );
 };
