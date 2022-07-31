51c6ed5deb936f65225acdaa0512d49911986d90
Sonia Amehmbo <87652337+xennia316@users.noreply.github.com>
2022-07-31 20:36:28 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import LodgingCard from "./LodgingCard/LodgingCard";
-// import SideNav from "./SideNav/SideNav";
 
 const App = () => {
   return (
     <section>
-      {/* <SideNav /> */}
       <LodgingCard />
     </section>
   );
