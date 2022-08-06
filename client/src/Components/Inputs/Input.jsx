65cfe7299aa8ea6dcb0152df1a9183d7db69133d
xennia316 <xennia316@gmail.com>
2022-08-06 22:53:24 +0100

    custom input realized

--- client/src/Components/Inputs/Input.jsx
+++ client/src/Components/Inputs/Input.jsx
@@ -5,8 +5,9 @@ import Styles from "./Input.module.css";
 const Input = () => {
   return (
     <section className={Styles.inputBody}>
-      <input className={Styles.actualInput} type="text" />
       <div className={Styles.sideIcon}>hello</div>
+      <input className={Styles.actualInput} type="text" />
+      <label className={Styles.label}>Name</label>
     </section>
   );
 };
