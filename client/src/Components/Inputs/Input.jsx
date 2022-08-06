1a6494632c51f5bf6c8e5feee898f746e39c821b
xennia316 <xennia316@gmail.com>
2022-08-06 23:09:24 +0100

    milestone

--- client/src/Components/Inputs/Input.jsx
+++ client/src/Components/Inputs/Input.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 import Styles from "./Input.module.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+
+
 const Input = () => {
   return (
     <section className={Styles.inputBody}>
-      <div className={Styles.sideIcon}>hello</div>
+      <div className={Styles.sideIcon}>
+        <FontAwesomeIcon icon={faLocationDot} className={Styles.icon} />
+      </div>
       <input className={Styles.actualInput} type="text" />
       <label className={Styles.label}>Name</label>
     </section>
