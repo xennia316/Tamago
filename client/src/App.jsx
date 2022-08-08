8af6a03d49658788623b9b4f6c10e234350946ee
xennia316 <xennia316@gmail.com>
2022-08-08 20:37:26 +0100

    adjusting inputs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Input from "./Components/Inputs/Input";
 import LodgingCard from "./Components/LodgingCard/LodgingCard";
+import MidSectionTop from "./Components/Sections/MidSectionTop";
 import SideNav from "./Components/SideNav/SideNav";
 import HotelCard from "./Components/SortHotelCard/HotelCard";
 
 const App = () => {
   return (
     <section>
-      <HotelCard />
+      {/* <HotelCard /> */}
       {/* <LodgingCard /> */}
       {/* <SideNav /> */}
       {/* <Input /> */}
+      <MidSectionTop />
     </section>
   );
 };
