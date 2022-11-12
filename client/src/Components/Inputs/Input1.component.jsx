0a107325cf18b547fff81a9469a191c0bb659a18
xennia316 <xennia316@gmail.com>
2022-11-12 17:20:39 +0100

    passing props

--- client/src/Components/Inputs/Input1.component.jsx
+++ client/src/Components/Inputs/Input1.component.jsx
@@ -5,19 +5,19 @@ import styles from "./Input1.module.css";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
-const Input1 = () => {
+const Input1 = ({ id, type, label }) => {
   return (
     <section className={`${styles.section}`}>
       <div className={`${styles.inputContainer}`}>
         <input
-          id="name"
+          id={id}
           className={`${styles.input}`}
-          type="text"
+          type={type}
           pattern=".+"
           required
         />
-        <label className={`${styles.label}`} for="name">
-          Nome
+        <label className={`${styles.label}`} for={label}>
+          {label}
         </label>
       </div>
     </section>
