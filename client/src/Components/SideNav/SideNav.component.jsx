06de81fe1e20d0a26a085870580f35a4efe5bb5d
xennia316 <xennia316@gmail.com>
2023-04-15 03:20:35 +0100

    clean file

--- client/src/Components/SideNav/SideNav.component.jsx
+++ client/src/Components/SideNav/SideNav.component.jsx
@@ -4,82 +4,20 @@ import styles from "./SideNav.module.css";
 // Importing icons from fontawesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faShareFromSquare,
-  faHeart,
-  faArrowAltCircleLeft,
-  faListAlt,
-  faRectangleXmark,
+	faShareFromSquare,
+	faHeart,
+	faArrowAltCircleLeft,
+	faListAlt,
+	faRectangleXmark,
 } from "@fortawesome/free-regular-svg-icons";
 
 import { faGear, faHomeAlt } from "@fortawesome/free-solid-svg-icons";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
 
 const SideNav = () => {
-  const [open, setOpen] = useState(true);
+	const [open, setOpen] = useState(true);
 
-  return (
-    <section className={styles.body}>
-      <section className={`${styles.topContainer}`}>
-        <section className={styles.top}>Tamago</section>
-        {open ? (
-          <FontAwesomeIcon
-            onClick={() => {
-              setOpen(!open);
-            }}
-            className={`${styles.toggleIcon}`}
-            icon={faRectangleXmark}
-          />
-        ) : (
-          <FontAwesomeIcon
-            onClick={() => {
-              setOpen(!open);
-            }}
-            className={`${styles.toggleIcon}`}
-            icon={faListAlt}
-          />
-        )}
-      </section>
-      {open ? (
-        <section className={styles.contents}>
-          <section className={styles.contents}>
-            <section className={styles.content}>
-              <FontAwesomeIcon icon={faHomeAlt} className={styles.icon} />
-              Dashboard
-            </section>
-            <section className={styles.content}>
-              <FontAwesomeIcon
-                icon={faShareFromSquare}
-                className={styles.icon}
-              />
-              Explore City
-            </section>
-            <section className={styles.content}>
-              <FontAwesomeIcon icon={faBell} className={styles.icon} />
-              Notification
-            </section>
-            <section className={styles.content}>
-              <FontAwesomeIcon icon={faHeart} className={styles.icon} />
-              Favorite
-            </section>
-            <section className={styles.content}>
-              <FontAwesomeIcon icon={faGear} className={styles.icon} />
-              Settings
-            </section>
-          </section>
-          <section></section>
-          <section className={styles.content}>
-            <FontAwesomeIcon
-              icon={faArrowAltCircleLeft}
-              className={styles.icon}
-            />
-            Logout
-          </section>
-        </section>
-      ) : (
-        <section></section>
-      )}
-    </section>
-  );
+	return <></>;
 };
 
 export default SideNav;
