13906703285bca7bffd42d8543aba1699b3a1f05
xennia316 <xennia316@gmail.com>
2023-04-15 03:20:12 +0100

    clean file

--- client/src/Components/LodgingCard/LodgingCard.component.jsx
+++ client/src/Components/LodgingCard/LodgingCard.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Styles from "./LodgingCard.module.css";
 
 import image from "../../images/hotel1.jpg";
 
@@ -7,24 +6,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const LodgingCard = () => {
-  return (
-    <section className={Styles.cardBody}>
-      <div className={Styles.imageDiv}>
-        <img src={image} className={Styles.image} alt="" />
-      </div>
-      <div className={Styles.hotelNameDiv}>
-        <h2 className={Styles.hotelName}>Shikara Hotel</h2>
-      </div>
-      <div className={Styles.hotelLocationDiv}>
-        <FontAwesomeIcon icon={faLocationDot} className={Styles.locationIcon} />
-        <span style={{ color: "#bcbcbc" }}>hello, welcome home</span>
-      </div>
-      <div className={Styles.hotelPriceDiv}>
-        <span className={Styles.hotelPrice}>$43.5</span>
-        <span style={{ color: "#bcbcbc" }}>/night</span>
-      </div>
-    </section>
-  );
+	return <></>;
 };
 
 export default LodgingCard;
