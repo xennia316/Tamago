bb57abb158ae48c814d15e4b203532570ed269e9
xennia316 <xennia316@gmail.com>
2023-04-16 07:38:57 +0100

    test tailwind

--- client/src/Components/Inputs/Input1.component.jsx
+++ client/src/Components/Inputs/Input1.component.jsx
@@ -6,7 +6,7 @@ import styles from "./Input1.module.css";
 // import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const Input1 = ({ id, type, label }) => {
-	return <></>;
+	return <section className="h-100"></section>;
 };
 
 export default Input1;
