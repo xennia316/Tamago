0aaf3678c0998903d6f6b8d27d5bc83e2c67d9c0
xennia316 <xennia316@gmail.com>
2024-04-05 17:51:37 +0100

    custom colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [],
+	theme: {
+		extend: {
+			colors: {
+				blue: "#4B88A2",
+				"primary-white": "#FFFAFB ",
+				"secondary-white": "#FFF9FB",
+				black: "#252627",
+			},
+		},
+	},
+	plugins: [],
+};
